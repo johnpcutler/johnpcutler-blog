0de4f7c91882bd3812cb63f7cd2e1755bda65e8f
John Cutler <johnpcutler@gmail.com>
2019-07-22 17:18:42 -0700

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'John Cutler',
   githubUrl: 'https://github.com/johnpcutler/johnpcutler-blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://johnpcutler-blog.netlify.com/',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'johncutlefish',
